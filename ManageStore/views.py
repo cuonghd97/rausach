847da066a0c7e4784aa6af766a44cb6b5ba26fbc
cuonghd97 <duccuongdc97@gmail.com>
2019-02-26 22:23:08 +0700

    cuong

--- ManageStore/views.py
+++ ManageStore/views.py
@@ -7,6 +7,7 @@ from django.contrib import messages
 
 # Create your views here.
 
+@decorators.login_required(login_url='/login/')
 def Base(request):
   if not request.user.is_authenticated:
     messages.warning(request, "Sai ten dang nhap hoac mat khau")
@@ -15,6 +16,10 @@ def Base(request):
 
 def Direct(request):
   return redirect("ManageStore:login")
+
+# Dashboard
+def Dashboard(request):
+  return render(request, 'dashboard.html')
 
 # Login
 class LoginClass(View):
@@ -28,7 +33,97 @@ class LoginClass(View):
       messages.warning(request, "Sai tên đăng nhập hoặc mật khẩu")
       return render(request, 'login.html')
     login(request, NhanVien)
-    return redirect('ManageStore:base')
+    return redirect('ManageStore:dashboard')
+
+# List Products
+@decorators.login_required(login_url='/login/')
+def products(request):
+  return render(request, 'products/list-products.html')
+
+# Set price
+@decorators.login_required(login_url='/login/')
+def set_price(request):
+  return render(request, 'products/set-price.html')
+
+# Inventory
+@decorators.login_required(login_url='/login/')
+def inventory(request):
+  return render(request, 'products/inventory.html')
+
+# Invoices
+@decorators.login_required(login_url='/login/')
+def invoice(request):
+  return render(request, 'exchange/invoices.html')
+
+# Purchase oroder
+@decorators.login_required(login_url='/login/')
+def purchase_order(request):
+  return render(request, 'exchange/purchase-order.html')
+
+# Purchase return
+@decorators.login_required(login_url='/login/')
+def pruchase_return(request):
+  return render(request, 'exchange/purchase-return.html')
+
+# Returns
+@decorators.login_required(login_url='/login/')
+def returns(request):
+  return render(request, 'exchange/returns.html')
+
+# Damage item
+@decorators.login_required(login_url='/login/')
+def damage_item(request):
+  return render(request, 'exchange/damage-item.html')
+
+# Cash book
+@decorators.login_required(login_url='/login/')
+def cash_book(request):
+  return render(request, 'cashbook/cash-book.html')
+
+# Customer
+@decorators.login_required(login_url='/login/')
+def customers(request):
+  return render(request, 'partner/customers.html')
+
+# Suppliers
+@decorators.login_required(login_url='/login/')
+def suppliers(request):
+  return render(request, 'partner/suppliers.html')
+
+# Customer report
+@decorators.login_required(login_url='/login/')
+def customer_report(request):
+  return render(request, 'reports/customer-report.html')
+
+# Employee report
+@decorators.login_required(login_url='/login/')
+def employee_report(request):
+  return render(request, 'reports/employee-report.html')
+
+# End of day report 
+@decorators.login_required(login_url='/login/')
+def end_of_day_report(request):
+  return render(request, 'reports/end-of-day-report.html')
+
+# Order report
+@decorators.login_required(login_url='/login/')
+def order_report(request):
+  return render(request, 'reports/order-report.html')
+
+# Product report 
+@decorators.login_required(login_url='/login/')
+def product_report(request):
+  return render(request, 'reports/product-report.html')
+
+# Sale report 
+@decorators.login_required(login_url='/login/')
+def sale_report(request):
+  return render(request, 'reports/sale-report.html')
+
+# Supplier report
+@decorators.login_required(login_url='/login/')
+def supplier_report(request):
+  return render(request, 'reports/supplier-report.html')
 
 # Logout
 def UserLogout(request):
